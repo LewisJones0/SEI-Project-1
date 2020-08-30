107e6da3382fc507da3ac97163c8670f543775e4
Lewis Jones <lewisjones0000@gmail.com>
2020-08-30 15:29:55 +0100

    Working on walls/coins etc.

--- scripts/app.js
+++ scripts/app.js
@@ -11,15 +11,27 @@ function init() {
   // Player starting positon
   let humanPosition = 74
 
-  // Add and Remove Player Functionality
 
-  function addHuman(position) {
-    cells[position].classList.add('humanSprite')
-  }
+  // ---------- Draw Map Function -------------
 
-  function removeHuman(position) {
-    cells[position].classList.remove('humanSprite')
-  }
+  const biblicLayout = new Object()
+  biblicLayout.WALL       = 0
+  biblicLayout.EMPTY      = 1
+  biblicLayout.STRENGTH   = 2
+  biblicLayout.BLOCK      = 3
+  biblicLayout.SWORD      = 4
+  biblicLayout.mapLayout  = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
 
   // Create Div Grid
 
@@ -29,24 +41,46 @@ function init() {
       cell.textContent = i
       grid.appendChild(cell)
       cells.push(cell)
+
+      //Add MapLayout
+      if (biblicLayout.mapLayout[i] === 0) {
+        cells[i].classList.add('wall1')
+      } else if (biblicLayout.mapLayout[i] === 1) {
+        cells[i].classList.add('wall1')
+      } else if (biblicLayout.mapLayout[i] === 2) {
+        cells[i].classList.add('wall1')
+      } else if (biblicLayout.mapLayout[i] === 3) {
+        cells[i].classList.add('wall1')
+      }
     }
     addHuman(startingPosition)
   }
 
-// ------- Handle PlayerInput --------
+  // Add and Remove Player Functionality
+
+  function addHuman(position) {
+    cells[position].classList.add('humanSprite')
+  }
+  
+  function removeHuman(position) {
+    cells[position].classList.remove('humanSprite')
+  }
+  
+
+  // ------- Handle PlayerInput --------
 
   function handleKeyUp(event) {
 
     removeHuman(humanPosition) // * remove Player from the current position
 
     const x = humanPosition % width // if Player / width has no remainder then dont move him left or right
-    const y = Math.floor(humanPosition / width) // vertical versionb
+    const y = Math.floor(humanPosition / width) // vertical version
     
     // Human Position and ArrowKeys/WASD
     switch (event.keyCode) { // * calculate the next position and update it
       case 39: //arrow right
         if (x < width - 1) 
-        humanPosition++ 
+          humanPosition++ 
         break
       case 37: //arrow left
         if (x > 0) humanPosition--
@@ -57,7 +91,7 @@ function init() {
       case 40: //arrow down
         if (y < width - 1) humanPosition += width
         break
-        case 68: //d right
+      case 68: //d right
         if (x < width - 1) humanPosition++
         break
       case 65: //a left
@@ -75,75 +109,56 @@ function init() {
   }
   createGrid(humanPosition)
 
-// ---------- Draw Map Function -------------
 
-const biblicLayout = new Object()
-biblicLayout.WALL       = 0;
-biblicLayout.EMPTY      = 1;
-biblicLayout.STRENGTH   = 2;
-biblicLayout.BLOCK      = 3;
-biblicLayout.SWORD      = 4;
-biblicLayout.mapLayout  = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    function drawMap(cells) {
-      const gridList = document.querySelectorAll('.grid div')
-      for (let i=0; i < gridList.length; i++) {
+  function drawMap() {
+    const gridList = document.querySelectorAll('.grid div')
+    for (let i = 0; i < gridList.length; i++) {
       if (biblicLayout.mapLayout[i] === 0) {
         console.log('working')
         // cells[position].classList.remove('humanSprite')
-        } else if (biblicLayout.mapLayout[i] === 1){
-          console.log('working2')
-        }
+      } else if (biblicLayout.mapLayout[i] === 1){
+        console.log('working2')
       }
     }
+  }
 
-    drawMap()
+  drawMap()
 
 
-    // Enemy Constructor
-    class Snake {
-      constructor(className, startIndex, speed) {
-        this.className = className //Snake Name/Class
-        this.startIndex = startIndex //Starting Position
-        this.speed = speed //Speed in ms
-        this.currentIndex = startIndex // Current Position
-        this.isScared = false //Player can kill
-        this.timerId = NaN  //Movement
-      }
+  // Enemy Constructor
+  class Snake {
+    constructor(className, startIndex, speed) {
+      this.className = className //Snake Name/Class
+      this.startIndex = startIndex //Starting Position
+      this.speed = speed //Speed in ms
+      this.currentIndex = startIndex // Current Position
+      this.isScared = false //Player can kill
+      this.timerId = NaN  //Movement
     }
-    // All Enemies 
-    const snakes = [
-      new Snake('Lust', 11, 250),
-      new Snake('Greed', 12, 250),
-      new Snake('Shame', 13, 250),
-      new Snake('Envy', 14, 250)
-    ]
-    console.log(snakes)
+  }
+  // All Enemies 
+  const snakes = [
+    new Snake('Lust', 11, 250),
+    new Snake('Greed', 12, 250),
+    new Snake('Shame', 13, 250),
+    new Snake('Envy', 14, 250)
+  ]
 
-    // Draw Snakes on map
-    snakes.forEach(snake => {
-      cells[Snake.currentIndex].classList.add(snake.className)
-      cells[Snake.currentIndex].classList.add('snake')
-    })
-    console.log(Snake)
+  // Draw Snakes on map
+  snakes.forEach(snake => {
+    cells[Snake.currentIndex].classList.add(snake.className)
+    cells[Snake.currentIndex].classList.add('snake')
+  })
+  console.log(Snake)
 
 
 
-// const backgroundArray = cells.map(eachDiv => {
-//   console.log(typeof eachDiv)
-//   return (eachDiv)
-// })
-// console.log(backgroundArray)
+  // const backgroundArray = cells.map(eachDiv => {
+  //   console.log(typeof eachDiv)
+  //   return (eachDiv)
+  // })
+  // console.log(backgroundArray)
 
 
 
